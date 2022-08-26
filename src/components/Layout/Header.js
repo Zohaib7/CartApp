8f2293520c09765cc613e8cb7d7202274e6a8f31
Zohaib7 <73638800+Zohaib7@users.noreply.github.com>
2022-08-26 21:44:49 +0500

    change

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <Fragment>
         <header className={classes.header}>
-            <h1>React Meals</h1>
+            <h1>React Mealsss</h1>
             <HeaderCartButton/>
         </header>
         <div className={classes['main-image']}>
